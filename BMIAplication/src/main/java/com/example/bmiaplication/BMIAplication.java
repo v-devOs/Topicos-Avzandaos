b641c41ca4a4d1f7d0b3c30a71939cb17fc93329
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-09-05 10:26:31 -0600

    Aplicación de BMi terminada

--- BMIAplication/src/main/java/com/example/bmiaplication/BMIAplication.java
+++ BMIAplication/src/main/java/com/example/bmiaplication/BMIAplication.java
@@ -1,34 +1,52 @@
 package com.example.bmiaplication;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Optional;
 
 public class BMIAplication extends Application {
+    TextField txtWeight = new TextField();
+    TextField txtHeight = new TextField();
+    Button btnCalcular = new Button("Calculate");
+    Button btnBorrar = new Button("Exit");
+    Text lblResults = new Text("Here goes the results...");
+
     @Override
     public void start(Stage stage) throws IOException {
 
 
-        Scene scene = new Scene(initGUI(), 320, 240);
+        Scene scene = new Scene(initGUI(stage), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
 
-    private StackPane initGUI(){
+    private StackPane initGUI(Stage stage){
+
+        // Imagen
+        Image image = new Image(getClass().getResource("/images/bmi.png").toString());
+        ImageView imageBMI = new ImageView();
+        imageBMI.setImage(image);
+        imageBMI.setFitWidth(200);
+        imageBMI.setFitHeight(100);
+        imageBMI.setPreserveRatio(true);
 
         StackPane root = new StackPane();
         GridPane gridPane = new GridPane();
@@ -40,43 +58,33 @@ public class BMIAplication extends Application {
         gridPane.add(title, 0, 0, 2, 1);
 
         Label lblWeight = new Label("Weight");
-        TextField txtWeight = new TextField();
         Label lblHeight = new Label("Height");
-        TextField txtHeight = new TextField();
 
         String styleLabels = "-fx-font-family: Verdana; -fx-font-size: 12px; -fx-text-fill: #dc0b82;";
         lblHeight.setStyle(styleLabels);
         lblWeight.setStyle(styleLabels);
 
-        Text lblResults = new Text("Here goes the results...");
         lblResults.setStyle("-fx-font-size: 15px;");
 
-        Button btnCalcular = new Button("Calcular");
-        Button btnBorrar = new Button("Borrar");
 
+        btnBorrar.setOnAction(handlerButtons);
+        btnCalcular.setOnAction(handlerButtons);
 
-        btnBorrar.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                deleteData(txtWeight, txtHeight, lblResults);
-            }
-        });
+        HBox hboxBtns = new HBox(2);
+        hboxBtns.setAlignment(Pos.CENTER);
 
-        btnCalcular.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                double IBM = calculateBMI(txtWeight.getText(), txtHeight.getText());
-                lblResults.setText(String.valueOf(IBM));
-            }
-        });
+        hboxBtns.getChildren().addAll( btnCalcular, btnBorrar );
+
+        btnCalcular.setMinWidth(150);
+        btnBorrar.setMinWidth(150);
 
         gridPane.add(lblWeight, 0, 1);
         gridPane.add(txtWeight, 1, 1);
         gridPane.add(lblHeight, 0, 2);
         gridPane.add(txtHeight, 1, 2);
         gridPane.add(lblResults, 0, 3, 2, 1);
-        gridPane.add(btnCalcular, 0, 4);
-        gridPane.add(btnBorrar, 1, 4);
+        gridPane.add( hboxBtns, 0, 4 , 2 , 1 );
+        gridPane.add( imageBMI, 2, 0, 1, 4);
 
 
         gridPane.setHgap(15);
@@ -90,18 +98,56 @@ public class BMIAplication extends Application {
         return root;
     }
 
-    private void deleteData(TextField txtWeight, TextField txtHeight, Text result){
-        txtWeight.setText("");
-        txtHeight.setText("");
-        result.setText("Here goes the results...");
+    private void closeWindows(Stage stage){
+        stage.close();
     }
 
-    private double calculateBMI(String weight, String height){
+    private double calculateBMI(Double weight, Double height){
 
-        double heightDouble = Double.parseDouble(height);
-        double iBM = Double.parseDouble(weight)/(heightDouble*heightDouble);
+        final DecimalFormat df = new DecimalFormat("0.00");
+        return Double.parseDouble(df.format(weight/(height*height)));
+    }
 
-        return iBM;
+    private String getBMICategory( double bmi){
+
+        String category = "UNDEFINED";
+
+        if( bmi < 18.5) category = "UNDERWEIGHT";
+        else if( bmi <= 24.9) category = "NORMAL";
+        else if( bmi <= 29.9) category = "OVERWEIGHT";
+        else if( bmi <= 39.9) category = "OBESE";
+        else category = "MORBIDLY OBESE";
+
+        return category;
+    }
+
+    EventHandler<ActionEvent> handlerButtons = ( event -> {
+
+        if( event.getSource() == btnCalcular ){
+            double weigth = Double.parseDouble(txtWeight.getText());
+            double heigth = Double.parseDouble(txtHeight.getText());
+
+            double bmi = calculateBMI( weigth, heigth );
+            String category = getBMICategory(bmi);
+
+            String results = "You BMI is " + bmi + ", with the category " + category;
+
+            lblResults.setText(results);
+        }
+        else{
+            confirmExit();
+        }
+    });
+
+    private void confirmExit(){
+        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmation.setTitle("Exit");
+
+        Optional<ButtonType> response = confirmation.showAndWait();
+
+        if( response.get() == ButtonType.OK){
+            Platform.exit();
+        }
     }
 
     public static void main(String[] args) {
